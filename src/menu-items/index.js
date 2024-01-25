4c92b3d1fc33f125bdf96586d9c1a4de5c79f7e0
priscafehiarisoa <mytyrealy@gmail.com>
2024-01-25 11:05:35 +0300

    annonce

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -1,11 +1,12 @@
 import dashboard from './dashboard';
 import pages from './pages';
 import Moteur from './Moteur';
+import AnnonceMenu from "./AnnonceMenu";
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard, pages, Moteur]
+  items: [dashboard, pages, Moteur,AnnonceMenu]
 };
 
 export default menuItems;
